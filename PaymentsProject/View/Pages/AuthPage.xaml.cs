1d49e98549249bb8cca53f40b7fe3fcf2554e4db
emk7online <emk7online@gmail.com>
2023-02-17 11:02:00 +0500

    Добавьте файлы проекта.

---
+++ PaymentsProject/View/Pages/AuthPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using PaymentsProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaymentsProject.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        Core db = new Core();
+        public AuthPage()
+        {
+            InitializeComponent();
+            List<Users> users = db.context.Users.ToList();
+            List<string> logins = new List<string>() { };
+            foreach (Users item in users)
+            {
+                logins.Add(item.login);
+            }
+            UserComboBox.ItemsSource = logins;
+            UserComboBox.SelectedIndex = 0;
+        }
+        //Проверка корректности пароля
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserPasswordBox.Password == db.context.Users.FirstOrDefault(x => x.login == UserComboBox.SelectedValue.ToString()).password)
+            {
+                this.NavigationService.Navigate(new MainPage());
+            }
+            else
+            {
+                MessageBox.Show("Введен не верный логин или пароль");
+            }
+            //Закрытие окна приложения
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void UserComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Properties.Settings.Default.ActiveUser = UserComboBox.SelectedValue.ToString();
+            Properties.Settings.Default.Save();
+        }
+    }
+}
